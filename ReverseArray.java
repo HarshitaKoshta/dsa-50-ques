70cff91eed8d56a8e736bad4bd6e7f07a6e36c25
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-05-06 17:28:59 +0530

    reverse array in place

---
+++ ReverseArray.java
@@ -0,0 +1,29 @@
+public class ReverseArray{
+    
+   static void reverse(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left<right) {
+            
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    
+    static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+       }
+    public static void main(String[] args) {
+        int[] myArray = {10, 20, 30, 40, 50};
+        reverse(myArray);
+        System.out.println("After reversing:");
+        printArray(myArray);
+    }
+
+}
