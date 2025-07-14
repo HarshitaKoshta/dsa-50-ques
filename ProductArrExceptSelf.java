ff216de345c2cea981e06d0e7ce63bb3c2a23a40
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-07-14 22:35:58 +0530

    product arr except self

--- ProductArrExceptSelf.java
+++ ProductArrExceptSelf.java
@@ -1,7 +1,7 @@
 class ProductArrExceptSelf{
     public static void main(String[] args) {
-        int[] a ={1,2,1,1,1};
-        int idx=a[0];
+        int[] a ={1,2,1,1,4};
+       // int idx=a[0];
         int product=1;
       
         for(int i =0; i<a.length; i++){
