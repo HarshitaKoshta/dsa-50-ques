e85c2e6050624d0fac5e6583aaac4bcabd1df30f
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-07-15 22:52:00 +0530

    palindrome check

---
+++ DSA Sheet/ValidPalindrome.java
@@ -0,0 +1,18 @@
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "racecars";
+        boolean palin  =true;
+        int st = s.charAt(0);
+        int ed = s.charAt(s.length()-1 );
+          for(int i=0;i<s.length();i++){
+             if(st!=ed){
+                palin = false;
+                break;
+             }
+          }
+          if(palin)
+        System.out.println("palindrome");
+    else
+        System.out.println("not palindrome");
+    }
+}
