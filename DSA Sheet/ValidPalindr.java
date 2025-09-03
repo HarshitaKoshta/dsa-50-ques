228932d156a83516de0816aa51a4d3e09da9f1a1
HarshitaKoshta <harshitakoshta2111@gmail.com>
2025-09-03 20:09:36 +0530

    valid palindrome

---
+++ DSA Sheet/ValidPalindr.java
@@ -0,0 +1,17 @@
+class ValidPalindr{
+    public static  boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
+            i++; j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("racecar"));
+        System.out.println(isPalindrome("heello"));
+
+    }
+}
